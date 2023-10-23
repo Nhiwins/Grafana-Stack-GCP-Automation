b14105f62977b5efd9ba3816e2c728c0c804bcf9
Lynn Nhi Nguyen <94258439+Nhiwins@users.noreply.github.com>
2023-10-23 15:51:26 -0700

    Update providers.tf with variables

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "google" {
   credentials = "file("/home/nnguyen/.config/gcloud/application_default_credentials.json")"
-  project     = "tf-gcp-project"
-  region      = "us-central1"
-  zone        = "us-central1-a"
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
   }
